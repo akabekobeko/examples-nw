6a45141ce119b6df45d545e034366434cbd1d771
akabeko <akabekobeko@gmail.com>
2015-05-18 23:46:31 +0900

    #6 gulp タスクのファイル分割を廃止して単一化した また、nom run による gulp タスクの隠蔽も実施。

--- audio-player/gulpfile.js
+++ audio-player/gulpfile.js
@@ -1,65 +1,155 @@
-// 汎用タスク定義
-require( 'require-dir' )( './gulp/tasks', { recurse: true } );
-
-////////////////////////////////////////////////////////////////////////////////
-//                           プロジェクト固有タスク
-////////////////////////////////////////////////////////////////////////////////
-
 var gulp = require( 'gulp' );
+var $    = require( 'gulp-load-plugins' )();
 
-/**
- * リリース用イメージを削除します。
- *
- * @param {Function} cb コールバック関数。
- */
-gulp.task( 'clean', function( cb ) {
-    var del = require( 'del' );
-    del( [ './bin/src', './bin/release' ], cb );
+// 共通タスク設定
+var common = {
+  src:        './src',
+  dest:       './release',
+  test:       './test',
+  isWatchify: false,
+  isUglify:   false
+};
+
+// browserify タスクのファイル監視モード実行フラグを有効化
+gulp.task( 'browserify-watchfy', function( done ) { common.isWatchify = true; done(); } );
+
+// browserify タスクの圧縮・最適化モード実行フラグを有効化
+gulp.task( 'browserify-uglify', function( done ) { common.isUglify = true; done(); } );
+
+// JavaScript 間の依存解決とコンパイルを実行し、その結果を単一のファイルとして出力する
+gulp.task( 'browserify', $.watchify( function( watchify ) {
+  var buffer    = require( 'vinyl-buffer' );
+  var formatter = require( 'pretty-hrtime' );
+  var time      = process.hrtime();
+
+  return gulp.src( [ common.src + '/js/App.js' ] )
+    .pipe( $.plumber() )
+    .pipe( watchify( {
+      watch: common.isWatchify,
+      basedir:   './',
+      debug:     true,
+      transform: [ 'babelify' ]
+    } ) )
+    .pipe( buffer() )
+    .pipe( $.sourcemaps.init( { loadMaps: true } ) )
+    .pipe( $.if( common.isUglify, $.uglify() ) )
+    .pipe( $.rename( 'bundle.js' ) )
+    .pipe( $.sourcemaps.write( './' ) )
+    .pipe( gulp.dest( common.src ) )
+    .on( 'end', function() {
+      var taskTime = formatter( process.hrtime( time ) );
+      $.util.log( 'Bundled', $.util.colors.green( 'bundle.js' ), 'in', $.util.colors.magenta( taskTime ) );
+    } );
+} ) );
+
+// Stylus コンパイルと結合
+gulp.task( 'stylus', function() {
+  return gulp.src( [
+      common.src + '/stylus/App.styl',
+      common.src + '/stylus/MusicList.styl',
+      common.src + '/stylus/Toolbar.styl',
+      common.src + '/stylus/Icon.styl'
+    ] )
+    .pipe( $.plumber() )
+    .pipe( $.sourcemaps.init() )
+    .pipe( $.stylus() )
+    .pipe( $.concat( 'bundle.css' ) )
+    .pipe( $.minifyCss() )
+    .pipe( $.sourcemaps.write( '.' ) )
+    .pipe( gulp.dest( common.src ) );
 } );
 
-/**
- * リリース用 CSS と、その参照を解決した HTML を生成します。
- */
-gulp.task( 'css-release', [ 'clean', 'css', ], function() {
-    var runSequence = require( 'run-sequence' );
-    runSequence( 'useref' );
+// アイコン フォント生成
+gulp.task( 'iconfont', function() {
+  return gulp.src( common.src + '/icons/*.svg' )
+    .pipe( $.iconfont( { fontName: 'icon' } ) )
+    .on( 'codepoints', function( codepoints ) {
+      var options = {
+        className: 'icon',
+        fontName:  'icon',
+        fontPath:  'fonts/',
+        cssFile:   'icon.css',
+        glyphs:    codepoints
+      };
+
+      // Stylus
+      gulp.src( common.src + '/icons/template.styl' )
+        .pipe( $.consolidate( 'lodash', options ) )
+        .pipe( $.rename( { basename: 'Icon' } ) )
+        .pipe( gulp.dest( common.src + '/stylus' ) );
+
+      // CSS ( 出力見本用 )
+      gulp.src( common.src + '/icons/template.css' )
+        .pipe( $.consolidate( 'lodash', options ) )
+        .pipe( $.rename( { basename: 'icon' } ) )
+        .pipe( gulp.dest( common.src ) );
+
+      // フォント出力見本 HTML ( CSS も必要 )
+      gulp.src( common.src + '/icons/template.html' )
+        .pipe( $.consolidate( 'lodash', options ) )
+        .pipe( $.rename( { basename: 'icon-sample' } ) )
+        .pipe( gulp.dest( common.src ) );
+    } )
+    .pipe( gulp.dest( common.src + '/fonts' ) );
 } );
 
-/**
- * リリース用イメージに必要なファイルをコピーします。
- * 
- * @return {Object} gulp ストリーム。
- */
-gulp.task( 'copy', [ 'js-release', 'css-release', 'iconfont' ], function() {
-    return gulp.src(
-            [ './src/js/bundle.js', 'src/package.json', './src/css/fonts/**' ],
-            { base: './src' }
-        )
-        .pipe( gulp.dest( './bin/src' ) );
+// アイコンフォントも含めた Stylus コンパイルと結合
+// これらは依存関係があるものの、watch の兼ね合いでタスクとしては依存設定したくない
+gulp.task( 'stylus-with-iconfont', [ 'iconfont' ], function( done ) {
+  var runSequence = require( 'run-sequence' );
+  runSequence( 'stylus' );
+  done();
 } );
 
-/**
- * ブロジェクトのリリース用イメージを出力します。
- */
-gulp.task( 'release', [ 'copy' ], function() {
-    var runSequence = require( 'run-sequence' );
-    runSequence( 'nw' );
+// リリース用イメージ削除
+gulp.task( 'clean', function( done ) {
+  var del = require( 'del' );
+  del( [ common.dest + '/src' ], done );
 } );
 
-/**
- * 開発用ビルドを実行します。
- */
-gulp.task( 'build', [ 'js', 'css', 'iconfont' ] );
-
-/**
- * 開発用リソースの変更を監視して、必要ならビルドを実行します。
- */
-gulp.task( 'watch', [ 'css', 'iconfont', 'watchify' ], function () {
-    gulp.watch( [ './src/stylus/*.styl', '!./src/css/*.css' ], [ 'css' ] );
-    gulp.watch( [ './src/icons/*.svg' ], [ 'iconfont' ] );
+// リリース用イメージの生成とコピー
+gulp.task( 'release-build', [ 'clean', 'browserify-uglify', 'browserify', 'stylus-with-iconfont' ], function() {
+  return gulp.src(
+      [
+        common.src + '/package.json',
+        common.src + '/index.html',
+        common.src + '/bundle.js',
+        common.src + '/bundle.css',
+        common.src + '/fonts/**'
+      ],
+      { base: common.src }
+    )
+    .pipe( gulp.dest(  common.dest + '/src' ) );
 } );
 
-/**
- * gulp の既定タスクです。
- */
+// nw.js ビルド
+gulp.task( 'nw', [ 'release-build' ], function() {
+  var builder = new ( require( 'node-webkit-builder' ) )( {
+    version:  '0.12.1',
+    files:     [ common.dest + '/src/**' ],
+    buildDir:  common.dest + '/bin',
+    cacheDir:  common.dest + '/nw',
+    platforms: [ 'osx64' ]
+  } );
+
+  builder.on( 'log', function( message ) {
+    $.util.log( 'node-webkit-builder', message );
+  } );
+
+  return builder.build().catch( function( err ) {
+    $.util.log( 'node-webkit-builder', err );
+  } );
+} );
+
+// ファイル監視
+gulp.task( 'watch', [ 'stylus-with-iconfont', 'browserify-watchfy', 'browserify' ], function () {
+  gulp.watch( [ common.src + '/stylus/*.styl' ], [ 'stylus'   ] );
+  gulp.watch( [ common.src + '/icons/*.svg' ],   [ 'iconfont' ] );
+} );
+
+gulp.task( 'test', function() {
+  gulp.src( [ common.test + '/*.js' ] );
+} );
+
+// 既定タスク
 gulp.task( 'default', [ 'watch' ] );
